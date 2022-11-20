f0d56abb5e2110f129b246baddad4710643ee017
Utsav Dhakal <udhakal970@gmail.com>
2022-11-20 15:39:05 +0545

    configured hibernate template

--- src/main/java/com/example/springorm/product/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/example/springorm/product/dao/impl/ProductDaoImpl.java
@@ -3,10 +3,16 @@ package com.example.springorm.product.dao.impl;
 import com.example.springorm.product.dao.ProductDao;
 import com.example.springorm.product.entity.Product;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
-    private HibernateTemplate hibernateTemplate;
+    private final HibernateTemplate hibernateTemplate;
+
+    public ProductDaoImpl(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
 
     @Override
     public int create(Product product) {
